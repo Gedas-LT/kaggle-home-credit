5e6ecb9223bb9066628a5675dbe5efcbcae3cce0
Gediminas Šimavičius <gediminas.simavicius@gmail.com>
2022-02-06 19:55:26 +0200

    Fixed FunctionTransformers

--- custom_transformers.py
+++ custom_transformers.py
@@ -6,16 +6,18 @@ def blend_organization_type(input_df: pd.DataFrame) -> pd.DataFrame:
     blended values of ORGANIZATION_TYPE feature.
     """
     
-    condlist = [input_df["ORGANIZATION_TYPE"].str.startswith("Business") == True,
-                input_df["ORGANIZATION_TYPE"].str.startswith("Trade") == True,
-                input_df["ORGANIZATION_TYPE"].str.startswith("Transport") == True,
-                input_df["ORGANIZATION_TYPE"].str.startswith("Industry") == True]
+    result = input_df.copy()
+    
+    condlist = [result["ORGANIZATION_TYPE"].str.startswith("Business") == True,
+                result["ORGANIZATION_TYPE"].str.startswith("Trade") == True,
+                result["ORGANIZATION_TYPE"].str.startswith("Transport") == True,
+                result["ORGANIZATION_TYPE"].str.startswith("Industry") == True]
 
     choicelist = ["Business", "Trade", "Transport", "Industry"]
 
-    input_df["ORGANIZATION_TYPE"] = np.select(condlist, choicelist, input_df["ORGANIZATION_TYPE"])
+    result["ORGANIZATION_TYPE"] = np.select(condlist, choicelist, result["ORGANIZATION_TYPE"])
     
-    return input_df
+    return result
 
 
 def credit_card_dpd(input_df: pd.DataFrame, credit_card_df: pd.DataFrame) -> pd.DataFrame:
@@ -27,13 +29,15 @@ def credit_card_dpd(input_df: pd.DataFrame, credit_card_df: pd.DataFrame) -> pd.
     credit_card_df -- additional dataframe with information about credit cards. 
     """
     
+    result = input_df.copy()
+    
     credit_card_dpd = credit_card_df[(credit_card_df["MONTHS_BALANCE"] > -12) & (credit_card_df["NAME_CONTRACT_STATUS"] == "Active")][["SK_ID_CURR", "SK_DPD"]]
     credit_card_dpd["FLAG_DPD"] = [1 if x > 0 else 0 for x in credit_card_dpd["SK_DPD"]]
     count_credit_card_dpd = credit_card_dpd.groupby("SK_ID_CURR")["FLAG_DPD"].sum().reset_index()
 
-    input_df = pd.merge(input_df, count_credit_card_dpd, on="SK_ID_CURR", how="left").fillna(0)
+    result = pd.merge(result, count_credit_card_dpd, on="SK_ID_CURR", how="left").fillna(0)
     
-    return input_df
+    return result
 
 
 def pos_cash_dpd(input_df: pd.DataFrame, pos_cash_df: pd.DataFrame) -> pd.DataFrame:
@@ -45,19 +49,23 @@ def pos_cash_dpd(input_df: pd.DataFrame, pos_cash_df: pd.DataFrame) -> pd.DataFr
     pos_cash_df -- additional dataframe with information about POS (point of sales) and cash loans. 
     """
     
+    result = input_df.copy()
+    
     pos_cash_dpd = pos_cash_df[(pos_cash_df["MONTHS_BALANCE"] > -12) & (pos_cash_df["NAME_CONTRACT_STATUS"] == "Active")].groupby("SK_ID_CURR")["SK_DPD"].sum().reset_index()
 
-    input_df = pd.merge(input_df, pos_cash_dpd, on="SK_ID_CURR", how="left").fillna(0)
+    result = pd.merge(result, pos_cash_dpd, on="SK_ID_CURR", how="left").fillna(0)
     
-    return input_df
+    return result
 
 
 def flag_insurance(input_df: pd.DataFrame) -> pd.DataFrame:
     """Takes in pandas dataframe from pipeline and returns input dataframe with FLAG_INSURANCE column."""
     
-    input_df["FLAG_INSURANCE"] = np.where(input_df["AMT_CREDIT"] - input_df["AMT_GOODS_PRICE"] > 0, 1, 0)
+    result = input_df.copy()
     
-    return input_df
+    result["FLAG_INSURANCE"] = np.where(result["AMT_CREDIT"] - result["AMT_GOODS_PRICE"] > 0, 1, 0)
+    
+    return result
 
 
 def credit_card_drawings(input_df: pd.DataFrame, credit_card_df: pd.DataFrame) -> pd.DataFrame:
@@ -69,24 +77,24 @@ def credit_card_drawings(input_df: pd.DataFrame, credit_card_df: pd.DataFrame) -
     credit_card_df -- additional dataframe with information about credit cards. 
     """
     
+    result = input_df.copy()
+    
     credit_card_drawings = credit_card_df[["SK_ID_CURR", "AMT_DRAWINGS_ATM_CURRENT", "AMT_DRAWINGS_CURRENT", "AMT_DRAWINGS_OTHER_CURRENT", "AMT_DRAWINGS_POS_CURRENT", "MONTHS_BALANCE"]].fillna(0)
     credit_card_drawings["ALL_DRAWINGS"] = credit_card_drawings[credit_card_drawings["MONTHS_BALANCE"] > -7].iloc[:,-4:].sum(axis=1)
     credit_card_drawings = credit_card_drawings.groupby("SK_ID_CURR")["ALL_DRAWINGS"].sum().reset_index()
-    input_df = pd.merge(input_df, credit_card_drawings, on="SK_ID_CURR", how="left").fillna(0)
+    result = pd.merge(result, credit_card_drawings, on="SK_ID_CURR", how="left").fillna(0)
     
-    return input_df
+    return result
 
 
 def flag_insurance(input_df: pd.DataFrame) -> pd.DataFrame:
     """Takes in pandas dataframe from pipeline and returns input dataframe with FLAG_INSURANCE column."""
     
-    bins = [0, 1, 365, 915, 2555, 5110, 9125, input_df["DAYS_EMPLOYED"].max()]
-    labels = ["YEAR_0", "YEAR_1", "YEAR_2.5", "YEAR_7", "YEAR_14", "YEAR_25", "YEAR_25_MORE"]
+    result = input_df.copy()
     
-    input_df["DAYS_EMPLOYED_BINS"] = pd.cut(input_df["DAYS_EMPLOYED"] , bins=bins, labels=labels, include_lowest=True).astype(object)
-    input_df["DAYS_EMPLOYED_BINS"] = pd.cut(input_df["DAYS_EMPLOYED"] , bins=bins, labels=labels, include_lowest=True).astype(object)
+    result["FLAG_INSURANCE"] = np.where(result["AMT_CREDIT"] - result["AMT_GOODS_PRICE"] > 0, 1, 0)
     
-    return input_df
+    return result
 
 
 def pandas_binning(input_df: pd.DataFrame, feature: str, bins: list, labels: list) -> pd.DataFrame:
@@ -100,6 +108,8 @@ def pandas_binning(input_df: pd.DataFrame, feature: str, bins: list, labels: lis
     labels -- list of labels used in pandas cut function. 
     """
     
-    input_df[feature] = pd.cut(input_df[feature] , bins=bins, labels=labels, include_lowest=True).astype(object)
+    result = input_df.copy()
     
-    return input_df
+    result[feature] = pd.cut(result[feature] , bins=bins, labels=labels, include_lowest=True).astype(object)
+    
+    return result
